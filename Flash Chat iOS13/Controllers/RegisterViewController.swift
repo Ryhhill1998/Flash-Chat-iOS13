f62ec443e364a5557c0c9df929c330998dad40d6
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-22 17:54:11 +0100

    Added register authentication and segue to chat

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        guard let email = emailTextfield.text else { return }
+        guard let password = passwordTextfield.text else { return }
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let e = error {
+                print(e)
+            } else {
+                self.performSegue(withIdentifier: "registerToChat", sender: self)
+            }
+        }
     }
-    
 }
