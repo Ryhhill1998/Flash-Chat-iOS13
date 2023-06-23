52653e39e03efb780c81a7c13575ea540b1a9c72
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-23 22:02:36 +0100

    Added different colours for different chats - sender/receiver

--- Flash Chat iOS13/Views/MessageCell.swift
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -13,16 +13,14 @@ class MessageCell: UITableViewCell {
     @IBOutlet weak var messageBubble: UIView!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
